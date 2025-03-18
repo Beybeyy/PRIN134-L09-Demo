6ba00ae30cd151aa5e5221fba4cb2b8c686df8e8
Beybeyy <cortez2231644@mls.ceu.edu.ph>
2025-03-18 12:08:09 +0800

    done check and reset btn

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// higlight function
 function highlightIngredients() {
     const ingredientsList = document.querySelectorAll("#recipe-ingredients li");
     ingredientsList.forEach((ingredient, index) => {
@@ -7,3 +8,30 @@ function highlightIngredients() {
       }
     });
   }
+  
+// check function
+function checkInstructions() {
+    const instructionsList = document.querySelectorAll("#recipe-instructions li");
+    let delay = 0; 
+    instructionsList.forEach(instruction => {
+        setTimeout(() => {
+            instruction.classList.add("is-done"); 
+        }, delay);
+        delay += 3000; 
+    });
+  }
+
+// reset function
+  function resetInstructions() {
+   
+    const ingredientsList = document.querySelectorAll("#recipe-ingredients li");
+    ingredientsList.forEach((ingredient) => {
+        ingredient.classList.remove("highlight");
+    });
+
+    
+    const instructionsList = document.querySelectorAll("#recipe-instructions li");
+    instructionsList.forEach((instruction) => {
+        instruction.classList.remove("is-done");
+    });
+}
